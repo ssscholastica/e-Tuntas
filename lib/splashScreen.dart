f29d15aab6cdc42a60c8e5d96194a59b591acf41
NabilaErwintria <nzaahiya@gmail.com>
2025-02-19 09:47:54 +0700

    update main

--- lib/splashScreen.dart
+++ lib/splashScreen.dart
@@ -1,1 +1,42 @@
+import 'dart:async';
 
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      Duration(seconds: 3),
+      () => Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => SplashScreen()),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              // margin: EdgeInsets.only(bottom: 50),
+              child: Image.asset("assets/logowebsite2.png", height: 300),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
