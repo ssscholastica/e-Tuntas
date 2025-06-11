2d35c453df75c0b31a391cf7db0624822d466913
ssscholastica <nakaishaputeri@gmail.com>
2025-06-11 12:46:58 +0700

    xxx

--- lib/services/fcm_service.dart
+++ lib/services/fcm_service.dart
@@ -3,9 +3,6 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:http/http.dart' as http;
-import 'package:flutter/material.dart';
-import 'package:etuntas/network/globals.dart';
 import 'dart:convert';
 
 class FCMService {
